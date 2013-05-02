cd9d7f8d22ba2fe83b2e46b47b233de2faaf8a4e
Lushi Li <lushili06@gmail.com>
2013-05-02 16:17:23 -0400

    add code to interpret lambda

--- shimmer.rb
+++ shimmer.rb
@@ -1,8 +1,8 @@
 class Env
   attr_accessor :context, :parent
-  def initialize(parent=nil)
+  def initialize(context={}, parent=nil)
     @parent = parent
-    @context = {}
+    @context = context
   end
 
   def find(var)
@@ -23,10 +23,12 @@ class Shimmer
 
   def add_globals
     @global_env.context.update({
-      cons: lambda { |x, y| y.unshift(x) }, car: lambda { |x| x[0] }, cdr: lambda { |x| x.shift },
+      cons: lambda { |x, y| [x] + y }, car: lambda { |x| x[0] }, cdr: lambda { |x| x.drop(1) },
       :+ => lambda { |x, y| x + y }, :* => lambda { |x, y| x * y }, :- => lambda { |x, y| x - y },
       :/ => lambda { |x, y| x / y }, :** => lambda { |x, y| x ** y }, :< => lambda { |x, y| x < y },
-      :> => lambda { |x, y| x > y }
+      :> => lambda { |x, y| x > y }, :>= => lambda { |x, y| x >= y }, :<= => lambda { |x, y| x <= y },
+      :'=' => lambda { |x, y| x == y }, equal?: lambda { |x, y| x == y }, null?: lambda { |x| x == []},
+      list: lambda { |x| Array(x) }, list?: lambda { |x| x.is_a? Array }
       })
   end
 
@@ -61,27 +63,26 @@ class Shimmer
       x
     elsif x.is_a? Symbol
       env.find(x).context[x] unless env.find(x).nil?
-    elsif x == :null
-      nil
-    elsif x[0] == :cons
-      _, exp1, exp2 = x
-      to_scheme(exp2.unshift(exp1))
     elsif x[0] == :quote
       _, exp = x
       to_scheme(exp)
     elsif x[0] == :set! #Not entirely sure this works properly. Need to revisit
       _, var, exp = x
-      env.find(var).context[var] = evaluate(exp)
+      env.find(var).context[var] = evaluate(exp, env)
     elsif x[0] == :define
       _, var, exp = x
-      env.context[var] = evaluate(exp)
+      env.context[var] = evaluate(exp, env)
     elsif x[0] == :if
       _, test, conseq, alt = x
-      evaluate(test) ? evaluate(conseq) : evaluate(alt)
+      evaluate(test, env) ? evaluate(conseq, env) : evaluate(alt, env)
+    elsif x[0] == :lambda
+      _, parms, exp = x
+      lambda { |*args| evaluate(exp, Env.new(Hash[parms.zip(args)], env))}
     else
-      exps = x.map { |exp| evaluate(exp) }
+      exps = x.map { |exp| evaluate(exp, env) }
       proc = exps.shift
       proc.call(*exps)
+
     end
   end
 
--- test.rb
+++ test.rb
@@ -15,10 +15,10 @@ class ShimmerTest < MiniTest::Unit::TestCase
 
   def test_define_var_num_2
     s = Shimmer.new
-    input = "(define x 4)"
-    s.evaluate(s.parse(input))
-    input = "x"
-    assert_equal(4, s.evaluate(s.parse(input)))
+    input1 = "(define x 4)"
+    s.evaluate(s.parse(input1))
+    input2 = "x"
+    assert_equal(4, s.evaluate(s.parse(input2)))
   end
 
   def test_quote
@@ -39,5 +39,33 @@ class ShimmerTest < MiniTest::Unit::TestCase
   def test_addition
     input = "(+ 2 3)"
     assert_equal(5, run_program(input))
+  end
+
+  def test_multiplication
+    input = "(* 2 3)"
+    assert_equal(6, run_program(input))
+  end
+
+  def test_subtraction
+    input = "(- 3 2)"
+    assert_equal(1, run_program(input))
+  end
+
+  def test_division
+    input = "(/ 3 2)"
+    assert_equal(1, run_program(input))
+  end
+
+  def test_exponent
+    input = "(** 3 2)"
+    assert_equal(9, run_program(input))
+  end
+
+  def test_lambda
+    s = Shimmer.new
+    input1 = "(define sum (lambda (a b) (+ a b)))"
+    s.evaluate(s.parse(input1))
+    input2 = "(sum 2 3)"
+    assert_equal(5, s.evaluate(s.parse(input2)))
   end
 end
